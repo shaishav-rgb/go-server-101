168cc4f7fea6b64e450491378f31b3bfd78dd092
shaishav-rgb <shaishavpaudel@gmail.com>
2026-05-02 09:26:34 +0545

    feat: add basic go server

--- main.go
+++ main.go
@@ -1,8 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter,r *http.Request){
+	if r.URL.Path!="/"{
+		http.NotFound(w,r)
+		return
+	}
+	w.Write([]byte("Hello from Server"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request){
+	if r.Method !=http.MethodPost{
+		w.Header().Set("Allow","POST")
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method not allowed"))
+		//using http.Error()
+		http.Error(w,"Method not allowed",http.StatusMethodNotAllowed)
+		return
+	}
+	//deleting default response header
+	// w.Header()["Date"]=nil
+	w.Write([]byte("Create a new Snippet"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request){
+	id,err:=strconv.Atoi(r.URL.Query().Get("id"))
+	if err!=nil || id<1{
+		http.NotFound(w,r)
+		return
+	}
+	// w.Write([]byte("Display a specific Snippet"))
+	fmt.Fprintf(w,"Display a specific snippet with ID %d...",id)
+}
 
 
 func main(){
-	fmt.Println("Hello world")
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/",home)
+	mux.HandleFunc("/snippet/view",snippetView)
+	mux.HandleFunc("/snippet/create",snippetCreate)
+
+	log.Print("Starting server on :4000")
+	err:=http.ListenAndServe(":4000",mux)
+	log.Fatal(err)
 }
